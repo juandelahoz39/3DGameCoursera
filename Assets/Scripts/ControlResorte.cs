3407e1c188c77bfea8e761fb0da3539ab7ee393a
juandelahoz39 <juan.delahoz39@gmail.com>
2020-07-12 20:18:35 -0500

    Se logro activar el accionador de resolte producto de la colisión con el domino

---
+++ Assets/Scripts/ControlResorte.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlResorte : MonoBehaviour {
+    Animator Anim;
+
+	// Use this for initialization
+	void Start () {
+        Anim = GetComponent<Animator> ();
+	}
+	
+    void OnCollisionEnter(Collision collision)
+    {
+        Anim.Play("Base Layer.Presionboton");
+        }
+
+    }
